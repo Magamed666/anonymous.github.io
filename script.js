5a7a22f3fc1939764af4caab65d1be6afb502846
Magamed666 <163529894+Magamed666@users.noreply.github.com>
2024-03-29 00:19:36 +0500

    Initial commit

--- script.js
+++ script.js
@@ -11,3 +11,24 @@ function closeContactForm() {
     popup.style.display = "none";
     overlay.style.display = "none";
 }
+function showPage(pageId) {
+    var pages = document.querySelectorAll('.content');
+    for (var i = 0; i < pages.length; i++) {
+        pages[i].style.display = 'none';
+    }
+    document.getElementById(pageId).style.display = 'block';
+}
+
+function openContactForm() {
+    var popup = document.getElementById("contactPopup");
+    var overlay = document.getElementById("overlay");
+    popup.style.display = "block";
+    overlay.style.display = "block";
+}
+
+function closeContactForm() {
+    var popup = document.getElementById("contactPopup");
+    var overlay = document.getElementById("overlay");
+    popup.style.display = "none";
+    overlay.style.display = "none";
+}
